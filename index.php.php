8c9780983ba8ccfeaf7682b408e6033e0ba14bc7
concepcionalabada <110779796+concepcionalabada@users.noreply.github.com>
2022-12-12 17:54:31 +0800

    Add files via upload

---
+++ index.php.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bio Data</title>
+</head>
+<body>
+	<center>
+
+		<table width="75%" border="1px solid"  rules="cols">
+			<tr>
+				<td align="center" width="750" height="100" ><h1>BIO-DATA</h1></td>
+				<td align="center" width="150" height="100" >ID<br>PHOTO</td>
+			</tr>
+		</table>
+		<table  width="75%" rules="rows">
+			<tr>
+				<td align="left"  bgcolor="black"><font color="white">PERSONAL DATA</font></td>
+				
+			</tr>
+		</table>
+		<table border="1px solid" rules="none">
+			<tr>
+				<td width="300" >Position Desired</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Date</td>
+		    	<td width="50">:<u>____________________________________</u></td>
+				
+			</tr>
+				<tr>
+				<td width="300" >Name</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left">Sex</td>
+		    	<td width="50">:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300">City Address</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left"><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Provincial Address</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Telephone</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left">Cellphone</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300">Email Address</td>
+		    	<td width="50">:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ></td>
+		    	<td width="50" ></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Date of Birth</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Place</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Civil Status</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Citizenship</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Height</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Weight</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Religion</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ></td>
+		    	<td width="50" ></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Spouse</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Occupation</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Name of Children</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="80" align="left" >Dateof Birth</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Father's Name</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Occupation</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Mother's Name</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Occupation</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300"  >Parent's Address</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ></td>
+		    	<td width="50" ></td>
+				
+			</tr>
+			<tr>
+				<td width="300"  >Language/ Dialect spoken & written</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Person to be contacted in case of emergency</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >His/Her Address and Telephone</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+		</table>
+		<table  width="75%" rules="rows">
+			<tr>
+				<td align="left"  bgcolor="black"><font color="white">EDUCATIONAL ATTAINMENT</font></td>
+				
+			</tr>
+		</table>
+		<table  border="1px solid" rules="none">
+			<tr>
+				<td width="300" >Elementary</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Year Graduated</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			
+			
+			
+			<tr>
+				<td width="300" >High School</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Year Graduated</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			
+			<tr>
+				<td width="300" >College</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >Year Graduated</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300"  >Degree Received</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Special Skills</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+		</table>
+		<!-- EMPLOYEMENT RECORDS -->
+		<table  width="75%" rules="none">
+			<tr>
+				<td align="left"  bgcolor="black"><font color="white">EMPLOYMENT RECORD</font></td>
+				
+			</tr>
+		</table>
+		
+		<table  border="1px solid" rules="none">
+			<tr>
+				<td width="300" >Company Name</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Position</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >From</td>
+		    	<td width="50" >:<u>______________</u>To:<u>___________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Company Name</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" ><u>_____________</u></td>
+		    	<td width="50" ><u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Position</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+		    	<td width="70" align="left" >From</td>
+		    	<td width="50" >:<u>______________</u>To:<u>___________________</u></td>
+				
+			</tr>
+			
+		</table>
+		<!-- CHARACTER REFERERENCE -->
+		<table  width="75%" rules="rows">
+			<tr>
+				<td align="left"  bgcolor="black"><font color="white">CHRACTER REFERENCE</font></td>
+				
+			</tr>
+		</table>
+		<table  border="1px solid" rules="none">
+			<tr>
+				<td width="300" >Name</td>
+		    	<td width="50" >:<u>_____________________________________</u></td>
+		    	<td width="80" align="left" >Company</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="300" >Position</td>
+		    	<td width="50" >:<u>_____________________________________</u></td>
+		    	<td width="70" align="left" >Contact No</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+			<tr>
+				<td width="300" >Name</td>
+		    	<td width="50" >:<u>_____________________________________</u></td>
+		    	<td width="70" align="left" >Company</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			<tr>
+				<td width="225" >Position</td>
+		    	<td width="40" >:<u>_____________________________________</u></td>
+		    	<td width="95" align="left" >Contact No</td>
+		    	<td width="50" >:<u>____________________________________</u></td>
+				
+			</tr>
+			
+			
+		</table>
+		<table width="75%" border="1px solid" rules="cols">
+			<tr>
+				<td width="50%" height= "150" >
+					<table>
+						<tr>
+							<td width="100"> Res.Cert.No</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+						<tr>
+							<td width="100"> Issued at</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+						<tr>
+							<td width="100"> Issued on</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+						<tr>
+							<td width="100"> SSS</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+						<tr>
+							<td width="100"> TIN</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+						<tr>
+							<td width="100"> NBI NO.</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+						<tr>
+							<td width="100"> Passport No</td>
+							<td width="40" >:<u>_____________________________________</u></td>
+						</tr>
+					</table>
+				</td>
+				<td width="50%"><br>
+					<p >
+						I hereby certify that the above information is true and <br> correct to the best of my knowledge and belief. I also<br>understand that any misinterpretation will be considered <br>sufficient reason for withdrawal of an offer or subsequent<br> dismissal if employed.
+
+						<br><br>
+						<center>
+						<u>_____________________________</u>
+						<br>Applicant's Signature</center> 
+					</p>
+					
+				</td>
+			</tr>
+		</table>
+	</center>
+</body>
+</html>
